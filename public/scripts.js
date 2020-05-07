19f62a3bbe97919f36051b0388e94cec2822d6fd
JerielZi <jeriel_zinga@hotmail.com>
2020-05-07 23:33:11 +0100

    Configuring the pagination in front-end

--- public/scripts.js
+++ public/scripts.js
@@ -7,5 +7,50 @@ for(item of menuItems) {
   }
 }
 
+//Paginação
+function paginate(selectedPage, totalPages) {
+  let pages = [], 
+    oldPage
+
+  for (let currentPage = 1; currentPage <= totalPages; currentPage++) {
+    
+    const firstAndLastPage = currentPage == 1 || currentPage == totalPages
+    const pagesAfterSelectedPage = currentPage <= selectedPage + 2
+    const pagesBeforeSelectedPage = currentPage >= selectedPage -2 
+
+    if (firstAndLastPage || pagesBeforeSelectedPage && pagesAfterSelectedPage) {
+
+      if (oldPage && currentPage - oldPage > 2) {
+        pages.push("...")
+      }
+
+      if (oldPage && currentPage - oldPage == 2) {
+        pages.push(oldPage + 1)
+      }
+      pages.push(currentPage)
+
+      oldPage = currentPage
+    } 
+  }
+  return pages
+}
+
+const pagination = document.querySelector(".pagination")
+const page = +pagination.dataset.page
+const total = +pagination.dataset.total
+const pages = paginate(page, total)
+
+let elements =""
+
+for (let page of pages) {
+  if(String(page).includes("...")) {
+    elements += `<span${page}">${page}</span>`
+  } else {
+    elements += `<a href="?page=${page}">${page}</a>`
+  }
+}
+
+pagination.innerHTML = elements
+
 
 
