74ffb5ac34cfe6f498f65b5022fb7c5b30a93e20
JerielZi <jeriel_zinga@hotmail.com>
2020-05-08 00:41:47 +0100

    Final adjustments

--- src/app/controllers/members.js
+++ src/app/controllers/members.js
@@ -4,10 +4,28 @@ const Member = require('../models/Member')
 
 module.exports = {
   index(req, res){
-    Member.all(function(members) {
-      return res.render("members/index", { members })
+    let { filter, page, limit } = req.query
 
-    })
+    page = page || 1
+    limit = limit || 2
+    let offset = limit * (page - 1)
+
+    const params = {
+      filter, 
+      page,
+      limit,
+      offset,
+      callback(members) {
+        const pagination = {
+          total: Math.ceil(members[0].total / limit) ,
+          page
+        }
+        return res.render("members/index", { members, pagination, filter })
+      }
+    }
+
+    Member.paginate(params)
+    
   },
 
   create(req, res){
@@ -72,6 +90,6 @@ module.exports = {
   delete(req, res){
     Member.delete(req.body.id, function() {
       return res.redirect(`/members`)
-    })
+    }) 
   }
 }
--- src/app/models/Member.js
+++ src/app/models/Member.js
@@ -110,5 +110,38 @@ module.exports = {
 
       callback(results.rows)
     })
+  },
+  paginate(params) {
+    const {filter, limit, offset, callback } = params
+
+    let query = "",
+        filterQuery ="",
+        totalQuery =` (
+          SELECT count(*) FROM members
+        ) AS total`
+
+    if (filter) {
+
+      filterQuery = `
+      WHERE members.name ILIKE '%${filter}%'
+      OR members.email ILIKE '%${filter}%'
+      `
+      totalQuery = `(
+        SELECT count(*) FROM members
+        ${filterQuery}
+      ) as total`
+    }
+
+    query = `
+    SELECT members.*, ${totalQuery}
+       FROM members
+       ${filterQuery}
+    LIMIT $1 OFFSET $2
+    `
+    db.query(query, [limit, offset], function(err, results) {
+      if (err) throw `Database Error! ${err}`
+
+      callback(results.rows)
+    })
   }
 }
